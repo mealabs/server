400177aabbd4dc4f352d52b4ea190ae88cfbb7f9
Akif Manzoor <akifmanzoor124@gmail.com>
2019-05-28 22:17:40 -0400

    updated code to reflect new headers sent from sensors

--- public/index.js
+++ public/index.js
@@ -4,6 +4,11 @@ function Refresh() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         response = JSON.parse(this.responseText)
+        document.getElementById("device_type").innerHTML = "Device Type: "+ response["Device Type"];
+        document.getElementById("device_name").innerHTML = "Device Name: "+ response["Device Name"];
+        document.getElementById("connection_type").innerHTML = "Connection Type: "+ response["Connection Type"];
+        document.getElementById("time_stamp").innerHTML = "Time Stamp: "+ response["Time Stamp"];
+        response = response["Sensor Data"]
         document.getElementById("temp_data").innerHTML = "Temperature: " + response.Temperature+"*C";
         document.getElementById("humid_data").innerHTML = "Humidity: " + response.Humidity+"%";
         }
